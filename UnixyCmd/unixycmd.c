fb3bdcc6b27a48165c8a5809074b53d6be44a462
Yoshio HANAWA <y@hnw.jp>
2016-07-20 16:21:41 +0900

    exitの置き換え用関数の名前をunixycmd_exitに変更

--- UnixyCmd/unixycmd.c
+++ UnixyCmd/unixycmd.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-
-static inline void exit(retval) {fflush(stdout);close(STDOUT_FILENO);fflush(stderr);close(STDERR_FILENO);pthread_exit((void *)retval);}
+#define exit unixycmd_exit
+static inline void unixycmd_exit(retval) {fflush(stdout);close(STDOUT_FILENO);fflush(stderr);close(STDERR_FILENO);pthread_exit((void *)retval);}
 
 int unixycmd_main(int argc, char *const *argv)
 {
